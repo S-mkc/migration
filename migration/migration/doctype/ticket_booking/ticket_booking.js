6dea437383eda9ac69ad50c275cd7f230ecfb6f2
S-mkc <jaissunny009@gmail.com>
2024-07-25 21:06:37 +0545

    changes made

--- migration/migration/doctype/ticket_booking/ticket_booking.js
+++ migration/migration/doctype/ticket_booking/ticket_booking.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2024, Mukesh and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Ticket Booking", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Ticket Booking", {
+	refresh(frm) {
+        frappe.msgprint("Hello")
+	},
+});
