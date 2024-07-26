739a5c66bc191ee0696b946ecde21554aa9144e5
S-mkc <jaissunny009@gmail.com>
2024-07-26 16:28:06 +0545

    expense approver added

---
+++ migration/public/js/expense_claim.js
@@ -0,0 +1,20 @@
+frappe.ui.form.on("Expense Claim", {
+    before_save(frm)
+    {
+        frappe.call({
+        method: 'migration.custom_code.expense_claim.add_approver',
+        args: {
+            owner: frm.doc.owner
+        },
+        freeze: true,
+        callback: (r) => {
+            frm.doc.expense_approver = r.message;
+            frm.refresh_fields();
+        },
+        error: (r) => {
+            console.log(r)
+        }
+        
+    })
+    },
+})
