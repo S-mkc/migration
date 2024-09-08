14e919a33c2bcf7a4cd8943c3c45650cc6e3e443
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2024-09-08 18:36:25 +0545

    Update out_of_office.js

--- migration/public/js/out_of_office.js
+++ migration/public/js/out_of_office.js
@@ -1,1 +1,46 @@
+frappe.ui.form.on("Out Of Office Slip", {
+onload_post_render: function(frm){
+	
+if (frm.doc.workflow_state == "Pending" && frm.doc.custom_approver_id != frappe.session.logged_in_user)
+	 {
+		$('.actions-btn-group').hide()
+	}
+	if ( frm.doc.workflow_state == "Draft" )
+	{
+		$('.actions-btn-group').show()
+	}
+  
+	// cur_frm.set_df_property('approval_status', 'hidden', 1);
+ //  cur_frm.set_df_property('expense_approver', 'hidden', 1);
+  cur_frm.refresh_fields();
+	
 
+},
+
+    before_save(frm)
+        {
+            frappe.call({
+            method: 'migration.custom_code.out_of_office_slip.add_approver',
+            args: {
+                owner: frm.doc.owner
+            },
+            freeze: true,
+            callback: (r) => {
+                frm.doc.custom_approver_id = r.message;
+                frm.refresh_fields();
+            },
+            error: (r) => {
+                console.log(r)
+            }
+            
+        })
+        },
+
+    // refresh(frm)
+    // {
+       
+    // },
+      
+
+    
+})
