17fe97a42ca1d608ad3aa4c1cb36c2c4cca24c3f
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2024-09-08 18:22:43 +0545

    Update out_of_office_slip.py

--- migration/custom_code/out_of_office_slip.py
+++ migration/custom_code/out_of_office_slip.py
@@ -1,1 +1,13 @@
+import frappe
+from frappe.utils import today
 
+def on_update(doc, method):
+    expense_approver = frappe.db.get_value("Employee", {"user_id":doc.owner}, "custom_approver_id")
+    if expense_approver == '' or expense_approver == None and "Administrator" not in frappe.get_roles():
+        frappe.throw("Please ask Administrator to set Purchase Approver For you")
+        
+@frappe.whitelist()
+def add_approver(owner):
+    expense_approver = frappe.db.get_value("Employee", {"user_id":owner}, "custom_approver_id")
+    if expense_approver != '' or expense_approver != None:
+        return expense_approver
