447c8cbed111a8cdd9fac1b56b2c83b7fb9e058b
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2024-09-09 19:22:05 +0545

    Update out_of_office_slip.py

--- migration/custom_code/out_of_office_slip.py
+++ migration/custom_code/out_of_office_slip.py
@@ -1,13 +1,27 @@
+# import frappe
+# from frappe.utils import today
+
+# def on_update(doc, method):
+#     expense_approver = frappe.db.get_value("Employee", {"user_id":doc.owner}, "custom_approver_id")
+#     if expense_approver == '' or expense_approver == None and "Administrator" not in frappe.get_roles():
+#         frappe.throw("Please ask Administrator to set Purchase Approver For you")
+        
+# @frappe.whitelist()
+# def add_approver(owner):
+#     expense_approver = frappe.db.get_value("Employee", {"user_id":owner}, "custom_approver_id")
+#     if expense_approver != '' or expense_approver != None:
+#         return expense_approver
 import frappe
 from frappe.utils import today
 
 def on_update(doc, method):
-    expense_approver = frappe.db.get_value("Employee", {"user_id":doc.owner}, "custom_approver_id")
-    if expense_approver == '' or expense_approver == None and "Administrator" not in frappe.get_roles():
-        frappe.throw("Please ask Administrator to set Purchase Approver For you")
+    expense_approver = frappe.db.get_value("Employee", {"user_id": doc.owner}, "custom_approver_id")
+    if (not expense_approver) and "Administrator" not in frappe.get_roles():
+        frappe.throw("Please ask Administrator to set Purchase Approver for you")
         
 @frappe.whitelist()
 def add_approver(owner):
-    expense_approver = frappe.db.get_value("Employee", {"user_id":owner}, "custom_approver_id")
-    if expense_approver != '' or expense_approver != None:
+    expense_approver = frappe.db.get_value("Employee", {"user_id": owner}, "custom_approver_id")
+    if expense_approver:
         return expense_approver
+    return None  # If no approver is found, return None
