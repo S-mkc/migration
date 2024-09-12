75dd9d52a7dad7699293fed016dff567ba250bd2
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2024-09-12 22:02:40 +0545

    Update install.py

--- migration/install.py
+++ migration/install.py
@@ -1,24 +1,47 @@
+# import frappe
+
+# @frappe.whitelist()
+# def create_salary_component():
+#     doc = frappe.new_doc("Salary Component")
+#     "doc.name" = "Basic Salary",
+#     "doc.salary_component_abbr" = "BASIC",
+#     "doc.type" = "Earning",
+#     "description" = "Basic salary component",
+#     "doc.depends_on_payment_days" = 0,
+#     "doc.is_tax_applicable" = 0,
+#     "doc.deduction_full_tax_on_selected_payroll_date" = 0,
+#     "doc.round_to_the_nearest_integer" = 0,
+#     "doc.statistical_component" = 0,
+#     "doc.do_not_include_in_total" = 0,
+#     "doc.remove_if_zero_valued" = 1,
+#     "doc.disabled" = 0,
+#     "doc.condition" = "",
+#     "doc.amount" = 50000,
+#     "doc.s_flexible_benefits" = 0
+#     doc.insert()
+#     doc.save()
+#     frappe.db.commit()
+
 import frappe
 
 @frappe.whitelist()
 def create_salary_component():
     doc = frappe.new_doc("Salary Component")
-    "doc.name" = "Basic Salary",
-    "doc.salary_component_abbr" = "BASIC",
-    "doc.type" = "Earning",
-    "description" = "Basic salary component",
-    "doc.depends_on_payment_days" = 0,
-    "doc.is_tax_applicable" = 0,
-    "doc.deduction_full_tax_on_selected_payroll_date" = 0,
-    "doc.round_to_the_nearest_integer" = 0,
-    "doc.statistical_component" = 0,
-    "doc.do_not_include_in_total" = 0,
-    "doc.remove_if_zero_valued" = 1,
-    "doc.disabled" = 0,
-    "doc.condition" = "",
-    "doc.amount" = 50000,
-    "doc.s_flexible_benefits" = 0
+    doc.name = "Basic Salary"
+    doc.salary_component_abbr = "BASIC"
+    doc.type = "Earning"
+    doc.description = "Basic salary component"
+    doc.depends_on_payment_days = 0
+    doc.is_tax_applicable = 0
+    doc.deduction_full_tax_on_selected_payroll_date = 0
+    doc.round_to_the_nearest_integer = 0
+    doc.statistical_component = 0
+    doc.do_not_include_in_total = 0
+    doc.remove_if_zero_valued = 1
+    doc.disabled = 0
+    doc.condition = ""
+    doc.amount = 50000
+    doc.s_flexible_benefits = 0
     doc.insert()
-    doc.save()
-    frappe.db.commit()
+    frappe.db.commit()  # Optional, only if you're handling multiple operations
 
