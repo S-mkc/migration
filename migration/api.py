f14b74c87ca76bf0ec8ff9f19e13bb24e67585b3
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2024-09-12 21:57:26 +0545

    Update api.py

--- migration/api.py
+++ migration/api.py
@@ -35,24 +35,3 @@ def get_nepali_date(date):
         return str(nepali_date)
 
 
-@frappe.whitelist()
-def create_salary_component():
-    doc = frappe.new_doc("Salary Component")
-    "doc.name" = "Basic Salary",
-    "doc.salary_component_abbr" = "BASIC",
-    "doc.type" = "Earning",
-    "description" = "Basic salary component",
-    "doc.depends_on_payment_days" = 0,
-    "doc.is_tax_applicable" = 0,
-    "doc.deduction_full_tax_on_selected_payroll_date" = 0,
-    "doc.round_to_the_nearest_integer" = 0,
-    "doc.statistical_component" = 0,
-    "doc.do_not_include_in_total" = 0,
-    "doc.remove_if_zero_valued" = 1,
-    "doc.disabled" = 0,
-    "doc.condition" = "",
-    "doc.amount" = 50000,
-    "doc.s_flexible_benefits" = 0
-    doc.insert()
-    doc.save()
-    frappe.db.commit()
