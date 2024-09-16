fcea523659542152718a1c8ccf91a26ee55ab14b
Mukesh <mukesh@yarsa.io>
2024-09-16 11:08:39 +0000

    bs date modified

---
+++ migration/public/js/nepali_date.js
@@ -0,0 +1,21 @@
+// Add a new field "Bikram Sambat Birthday" to the Employee. Then add the following "Client Script" to ErpNext.
+
+frappe.ui.form.on('Fiscal Year', {
+    // 'date_of_birth': set_bs_date,
+    // 'refresh': set_bs_date
+
+set_bs_date (frm) {
+    frappe.call({ 
+        type: 'GET',
+        method: 'bs-date-utils', 
+        args: { method: 'ad_to_bs', date: frm.doc.date_of_birth},
+        
+        callback: function(response) {
+            if(response.message.length === 10) {
+                frm.set_value('year_start_date', response.message);
+                frm.set_value('year_end_date', response.message);
+            }
+        }
+    });
+}
+})
