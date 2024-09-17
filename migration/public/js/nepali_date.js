0a44cb8177c23d93634ec0b22f7e0a9a14ffcb95
Mukesh <mukesh@yarsa.io>
2024-09-17 04:21:08 +0000

    file midified

--- migration/public/js/nepali_date.js
+++ migration/public/js/nepali_date.js
@@ -1,23 +1,23 @@
-// Add a new field "Bikram Sambat Birthday" to the Employee. Then add the following "Client Script" to ErpNext.
+// // Add a new field "Bikram Sambat Birthday" to the Employee. Then add the following "Client Script" to ErpNext.
 
-frappe.ui.form.on('Fiscal Year', {
-    // 'date_of_birth': set_bs_date,
-    // 'refresh': set_bs_date
+// frappe.ui.form.on('Fiscal Year', {
+//     // 'date_of_birth': set_bs_date,
+//     // 'refresh': set_bs_date
 
-refresh: function (frm) {
-    frappe.call({ 
-        type: 'GET',
-        method: 'migration.api.ad_to_bs', 
-        args: { date: frm.doc.date_of_birth },
-        // args: { method: 'ad_to_bs', date: frm.doc.date_of_birth},
-        // args: { method: 'ad_to_bs'},
-        freeze: true,
-        callback: function(response) {
-            if(response.message.length === 10) {
-                frm.set_value('year_start_date', response.message);
-                frm.set_value('year_end_date', response.message);
-            }
-        }
-    });
-}
-})
+// refresh: function (frm) {
+//     frappe.call({ 
+//         type: 'GET',
+//         method: 'migration.api.ad_to_bs', 
+//         args: { date: frm.doc.date_of_birth },
+//         // args: { method: 'ad_to_bs', date: frm.doc.date_of_birth},
+//         // args: { method: 'ad_to_bs'},
+//         freeze: true,
+//         callback: function(response) {
+//             if(response.message.length === 10) {
+//                 frm.set_value('year_start_date', response.message);
+//                 frm.set_value('year_end_date', response.message);
+//             }
+//         }
+//     });
+// }
+// })
