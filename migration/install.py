d9a021cbeecb74801c924c6e87a23e44475f8792
Mukesh <mukesh@yarsa.io>
2024-09-17 04:23:37 +0000

    file midified

--- migration/install.py
+++ migration/install.py
@@ -2,7 +2,38 @@ import frappe
 import datetime
 # import nepali_datetime
 
-from migration.api import ad_to_bs 
+# from migration.api import ad_to_bs 
+    # 
+# from nepali_date import NepaliDate  # Import the Nepali date library
+
+# def create_fiscal_year():
+#     # Example of creating a fiscal year with Nepali date support
+#     fiscal_year = frappe.get_doc({
+#         "doctype": "Fiscal Year",
+#         "name": "Fiscal Year 2079/80",  # Example name
+#         "start_date": "2022-04-14",  # Gregorian start date
+#         "end_date": "2023-04-13",  # Gregorian end date
+#         "nepali_start_date": nepali_datetime(2079, 1, 1).to_gregorian(),  # Convert Nepali to Gregorian
+#         "nepali_end_date": nepali_datetime(2080, 1, 1).to_gregorian(),  # Convert Nepali to Gregorian
+#     })
+#     fiscal_year.insert()
+# def create_fiscal_year():
+#     today_ad = datetime.date.today()
+#     company = frappe.get_all("Company", fields=["name"])
+#     doc = frappe.new_doc("Fiscal Year")
+#     doc.year = "Nepal Fiscal Year"
+#     doc.disabled = 0
+#     doc.is_short_year = 0
+#     # doc.year_start_date = '2025-07-16'
+#     doc.year_start_date = ad_to_bs(today_ad)  # Convert today's date to BS for start date
+#     # doc.year_end_date = '2026-07-15'
+#     doc.year_end_date = ad_to_bs(frappe.utils.add_days(today_ad, 364))  # Convert end date to BS
+#     doc.append("companies", {
+#         "company" : company,
+#         # "account" : "Salary - Y"
+#     })
+#     doc.insert()
+
 @frappe.whitelist()
 def create_salary_component():
     company = frappe.get_all("Company", fields=["name"])
@@ -55,39 +86,9 @@ def create_salary_component():
     doc.s_flexible_benefits = 0
     doc.insert()
     # frappe.db.commit()
-    # 
-# from nepali_date import NepaliDate  # Import the Nepali date library
-
-# def create_fiscal_year():
-#     # Example of creating a fiscal year with Nepali date support
-#     fiscal_year = frappe.get_doc({
-#         "doctype": "Fiscal Year",
-#         "name": "Fiscal Year 2079/80",  # Example name
-#         "start_date": "2022-04-14",  # Gregorian start date
-#         "end_date": "2023-04-13",  # Gregorian end date
-#         "nepali_start_date": nepali_datetime(2079, 1, 1).to_gregorian(),  # Convert Nepali to Gregorian
-#         "nepali_end_date": nepali_datetime(2080, 1, 1).to_gregorian(),  # Convert Nepali to Gregorian
-#     })
-#     fiscal_year.insert()
-def create_fiscal_year():
-    today_ad = datetime.date.today()
-    company = frappe.get_all("Company", fields=["name"])
-    doc = frappe.new_doc("Fiscal Year")
-    doc.year = "Nepal Fiscal Year"
-    doc.disabled = 0
-    doc.is_short_year = 0
-    # doc.year_start_date = '2025-07-16'
-    doc.year_start_date = ad_to_bs(today_ad)  # Convert today's date to BS for start date
-    # doc.year_end_date = '2026-07-15'
-    doc.year_end_date = ad_to_bs(frappe.utils.add_days(today_ad, 364))  # Convert end date to BS
-    doc.append("companies", {
-        "company" : company,
-        # "account" : "Salary - Y"
-    })
-    doc.insert()
 
 def install():
     create_salary_component()
-    create_fiscal_year()
+    # create_fiscal_year()
 
     # return create_salary_component()
