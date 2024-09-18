3e7067d5086ce145b925fe4c95e544508f7fd98d
Mukesh <mukesh@yarsa.io>
2024-09-18 06:12:25 +0000

    install.py modified

--- migration/install.py
+++ migration/install.py
@@ -171,6 +171,120 @@ def create_salary_component():
     salary_component_names.append(doc.salary_component)  # Add to list
     doc.save() 
 
+    #Gratuity
+    doc = frappe.new_doc("Salary Component")
+    doc.salary_component = "Gratuity"
+    doc.salary_component_abbr = "Gr"
+    doc.type = "Earning"
+    doc.description = "Graturity Earning Based on Formula"
+    doc.depends_on_payment_days = 0
+    doc.is_tax_applicable = 0
+    doc.deduction_full_tax_on_selected_payroll_date = 0
+    doc.round_to_the_nearest_integer = 0
+    doc.statistical_component = 0
+    doc.do_not_include_in_total = 0
+    doc.remove_if_zero_valued = 1
+    doc.disabled = 0
+    doc.append("accounts", {
+        "company" : company,
+        "account" : "Salary - Y"
+    })
+    doc.condition = ""
+    doc.amount_based_on_formula = 1
+    doc.formula = "BASIC * .0833"
+    doc.formula_read_only = 1
+    doc.s_flexible_benefits = 0
+    salary_component_names.append(doc.salary_component)  # Add to list
+    doc.save() 
+
+    #Gratuity Deduction 
+    doc = frappe.new_doc("Salary Component")
+    doc.salary_component = "Gratuity Deduction"
+    doc.salary_component_abbr = "Gr_D"
+    doc.type = "Deduction"
+    doc.description = "Graduity Deduction Based on Formula"
+    doc.depends_on_payment_days = 0
+    doc.is_tax_applicable = 0
+    doc.deduction_full_tax_on_selected_payroll_date = 0
+    doc.round_to_the_nearest_integer = 0
+    doc.statistical_component = 0
+    doc.do_not_include_in_total = 0
+    doc.remove_if_zero_valued = 1
+    doc.disabled = 0
+    doc.append("accounts", {
+        "company" : company,
+        "account" : "Salary - Y"
+    })
+    doc.condition = ""
+    doc.amount_based_on_formula = 1
+    doc.formula = "BASIC * .0833"
+    doc.formula_read_only = 1
+    doc.s_flexible_benefits = 0
+    salary_component_names.append(doc.salary_component)  # Add to list
+    doc.save() 
+
+    #Other Allowance 
+    doc = frappe.new_doc("Salary Component")
+    doc.salary_component = "Other Allowance"
+    doc.salary_component_abbr = "OA"
+    doc.type = "Earning"
+    doc.description = "Other Allowance based on Formula"
+    doc.depends_on_payment_days = 0
+    doc.is_tax_applicable = 0
+    doc.deduction_full_tax_on_selected_payroll_date = 0
+    doc.round_to_the_nearest_integer = 0
+    doc.statistical_component = 0
+    doc.do_not_include_in_total = 0
+    doc.remove_if_zero_valued = 1
+    doc.disabled = 0
+    doc.append("accounts", {
+        "company" : company,
+        "account" : "Salary - Y"
+    })
+    doc.condition = ""
+    doc.amount_based_on_formula = 1
+    doc.formula = "ctc-BASIC"
+    # doc.set_df_property('formula', 'read_only', 1)
+    # doc.fields_dict['formula'].read_only = 1
+    # doc.set_df_property('formula', 'hidden', 1)
+    doc.formula_read_only = 1
+    doc.s_flexible_benefits = 0
+    salary_component_names.append(doc.salary_component)  # Add to list
+    doc.save()
+
+    #Leave and Late Deduction
+    doc = frappe.new_doc("Salary Component")
+    doc.salary_component = "Leave and Late Deduction"
+    doc.salary_component_abbr = "LLD"
+    doc.type = "Deduction"
+    doc.description = "Leave and Late Deduction Free Field"
+    doc.depends_on_payment_days = 0
+    doc.is_tax_applicable = 0
+    doc.deduction_full_tax_on_selected_payroll_date = 0
+    doc.round_to_the_nearest_integer = 0
+    doc.statistical_component = 0
+    doc.do_not_include_in_total = 0
+    doc.remove_if_zero_valued = 1
+    doc.disabled = 0
+    doc.append("accounts", {
+        "company" : company,
+        "account" : "Salary - Y"
+    })
+    doc.condition = ""
+    doc.amount_based_on_formula = 0
+    doc.s_flexible_benefits = 0
+    salary_component_names.append(doc.salary_component)  # Add to list
+    doc.save()
+
+    #Income Tax Unmarried
+    # doc = frappe.new_doc("Salary Component")
+    # doc.salary_component = "Income Tax (Unmarried)"
+    # doc.salary_component_abbr = "IT_U"
+    # doc.type = 'Deduction'
+    # doc.description = "Income Tax as per nepal law"
+    # doc.depends_on_payment_days = 0
+
+        
     return salary_component_names  # Return the list of created component names
 
 def install():
---
+++ migration/public/js/salary_slip.js
@@ -0,0 +1,56 @@
+frappe.ui.form.on('Salary Slip', {
+    refresh: function(frm) {
+        calculate_selected_earnings(frm);
+    },
+    earnings_add: function(frm) {
+        calculate_selected_earnings(frm);
+    },
+    earnings_remove: function(frm) {
+        calculate_selected_earnings(frm);
+    },
+    earnings_on_form_rendered: function(frm) {
+        calculate_selected_earnings(frm);
+    },
+    deductions_add: function(frm) {
+        calculate_selected_earnings(frm);
+    },
+    deductions_remove: function(frm) {
+        calculate_selected_earnings(frm);
+    },
+    validate: function(frm) {
+        calculate_selected_earnings(frm);
+    },
+    // before_save: function(frm) {
+    //     calculate_selected_earnings(frm);
+    // },
+
+});
+
+function calculate_selected_earnings(frm) {
+    let total_selected_earnings = 0;
+    let total_deductions = 0;
+    // const selected_components = ['Basic', 'Grade Amount', 'Other Allowances'];
+    const selected_components = ['Basic', 'Other Allowances', 'Grade Amount', 'Blog Allowance', 'Previous Month Adjustment Earning']; 
+    // const deduction_components = ['PF (Employee)', 'Insurance Premium'];
+    const deduction_components = ['PF (Employee)', 'Insurance Premium', 'Leave and Late Deduction', 'CIT Deduction', 'Previous Month Adjustment Deduction'];
+
+    frm.doc.earnings.forEach(function(row) {
+        if (selected_components.includes(row.salary_component)) {
+            total_selected_earnings += row.amount;
+        }
+    });
+
+    frm.doc.deductions.forEach(function(row) {
+        if (deduction_components.includes(row.salary_component)) {
+            total_deductions += row.amount;
+        }
+    });
+
+    let taxable_salary = total_selected_earnings - total_deductions;
+
+    frm.set_value('custom_taxable_salary', taxable_salary);
+    cur_frm.refresh_field('custom_taxable_salary');
+    cur_frm.trigger('Income Tax');
+    cur_frm.trigger('Income Tax (Married)');
+    
+}
