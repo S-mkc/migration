0fbb713ce15fec25bc1c6462a2868da882b26d22
Mukesh <mukesh@yarsa.io>
2024-10-23 04:04:35 +0000

    filed deleted

--- migration/migration/report/purchase_register_vat/purchase_register.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2024, raindrop and contributors
-// For license information, please see license.txt
-/* eslint-disable */
-
-frappe.query_reports["Purchase Register"] = {
-	"filters": [
-
-	]
-};
